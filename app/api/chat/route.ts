eb28c723ca808797def5ffdf00b7682a599d4485
sidhantbishnoi12 <sidhantbishnoi@gmail.com>
2025-11-27 17:30:05 +0530

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,4 +1,4 @@
-
+//new
 import { streamText, UIMessage, convertToModelMessages, stepCountIs, createUIMessageStream, createUIMessageStreamResponse } from 'ai';
 import { MODEL } from '@/config';
 import { SYSTEM_PROMPT } from '@/prompts';
