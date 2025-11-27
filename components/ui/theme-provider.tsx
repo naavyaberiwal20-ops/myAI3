0141ec0d3a2caee89a115605853ce10e91240e85
sidhantbishnoi12 <sidhantbishnoi@gmail.com>
2025-11-27 16:52:13 +0530

    Update theme-provider.tsx

--- components/ui/theme-provider.tsx
+++ components/ui/theme-provider.tsx
@@ -1,1 +1,17 @@
-"use client"; import * as React from "react"; import { ThemeProvider as NextThemesProvider } from "next-themes"; export function ThemeProvider({ children }: { children: React.ReactNode }) { return ( <NextThemesProvider attribute="class" defaultTheme="system" enableSystem={true} disableTransitionOnChange > {children} </NextThemesProvider> ); }
+"use client"; 
+
+import * as React from "react"; 
+import { ThemeProvider as NextThemesProvider } from "next-themes"; 
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) { 
+  return ( 
+    <NextThemesProvider 
+      attribute="class" 
+      defaultTheme="system" 
+      enableSystem={true} 
+      disableTransitionOnChange 
+      > 
+      {children} 
+    </NextThemesProvider> 
+  ); 
+}
